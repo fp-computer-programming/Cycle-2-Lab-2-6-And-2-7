014168b3ff2bbbf9b18aac33ece4c51c62f35477
Michael Ogunwale <144066645+MOgunwale@users.noreply.github.com>
2023-09-20 11:27:13 -0400

    Update file.py

--- file.py
+++ file.py
@@ -1,19 +1,8 @@
-# Create a Python file named lab_2-5.py
+# Create a Python file named lab_2-6.py
 
-"""
-For each of the following assignment statements copy the statement as a comment, 
-then write the other way you could write the statement using augmented assignments 
-or not using augmented assignments. To start, set all variables used equal to 21. 
-"""
+# Set a = 5, b = 10, c = 0, d = -4
 
-"""
- x += 5
- y = y - 3
- z **= 2
- w = w % 2
- t = t / 5
- a *= 12
- i = i // 7
-"""
+# Create a conditional statement to determine if each variable is even or odd. 
+ 
+# Print the results.
 
-# Be sure to include comments above each pair of 2 lines with the question number (Ex. # Question 1) and print statements beneath each assignment.
